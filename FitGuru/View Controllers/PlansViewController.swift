5da9827e859fcbfeeba2bc1c6cbc8609c88a93d6
AnthonyCampana <acampana18@gmail.com>
2021-05-21 08:37:45 -0400

    comment are added to database

--- FitGuru/View Controllers/PlansViewController.swift
+++ FitGuru/View Controllers/PlansViewController.swift
@@ -42,21 +42,46 @@ class PlansViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject])  ?? []
+        
+        return comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+       
+        //here
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject])  ?? []
         
-        let post = posts[indexPath.row]
+        //here
+        if indexPath.row == 0 {
         
-        let user = post["author"] as! PFUser
-        cell.fitguruLabel.text = user.username
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            let user = post["author"] as! PFUser
+            cell.fitguruLabel.text = user.username
+            
+            cell.workoutLabel.text = post["workout"] as? String
+            cell.dietLabel.text = post["diet"] as? String
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            return cell
+        }
         
-        cell.workoutLabel.text = post["workout"] as? String
-        cell.dietLabel.text = post["diet"] as? String
-        
-        return cell
     }
 
     
@@ -68,6 +93,26 @@ class PlansViewController: UIViewController, UITableViewDelegate, UITableViewDat
                 delegate.window?.rootViewController = firstViewController
     }
     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //here
+        let post = posts[indexPath.section]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a test comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("error saving comment")
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
